80610faadeb2e0a2b0fc8d75ff7a0ea0af71aa83
Rowan DeLong <rowanwinzer@gmail.com>
2021-09-05 11:07:02 -0600

    add methods to handle view logic

---
+++ spec/features/admin/shelters/show_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe "admin shelter show page" do
+
+  it "shows each shelter" do
+
+  end
+
+end
--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -5,4 +5,43 @@ RSpec.describe Application, type: :model do
     it {should have_many :application_pets}
     it {should have_many(:pets).through(:application_pets)}
   end
+
+  describe 'validations' do
+    it {should validate_presence_of(:name)}
+  end
+
+  describe "instance methods" do
+
+    before :each do
+      @shelter = Shelter.create!(name: 'Mystery Building', city: 'Irvine CA', foster_program: false, rank: 9)
+      @scooby = Pet.create!(name: 'Scooby', age: 2, breed: 'Great Dane', adoptable: true, shelter_id: @shelter.id)
+      @salem = Pet.create!(name: 'Salem', age: 16, breed: 'Cat', adoptable: true, shelter_id: @shelter.id)
+
+      @application = Application.create!(name: "P. Sherman", street_address: "42 Wallaby way",
+        city: "Denver", state: "CO", zip_code: "80202")
+
+      @application_2 = Application.create!(name: "Wall-e", street_address: "42 Wallaby way",
+        city: "Denver", state: "CO", zip_code: "80202", app_status: "Pending")
+
+      @application_3 = Application.create!(name: "Kyle", street_address: "42 Wallaby way",
+        city: "Denver", state: "CO", zip_code: "80202")
+    end
+
+    describe  '#show_pet_search?' do
+      it "can determine if pet search should show" do
+        expect(@application.show_pet_search?).to be true
+        expect(@application_2.show_pet_search?).to be false
+      end
+    end
+
+    describe  '#show_app_submit?' do
+      it "can determine if app submit should show" do
+        expect(@application.show_app_submit?).to be false
+
+        app_pet_1 = ApplicationPet.create!(application_id: @application_3.id, pet_id: @scooby.id)
+
+        expect(@application_3.show_app_submit?).to be true
+      end
+    end
+  end
 end
